2e6d04bc2887466cfc10d8834b8bb602962858e2
Tomas Vodrazka <tomas.vodrazka@gmail.com>
2019-02-25 10:19:36 +0100

    replacing quotes, undefined variable are replaced with an empty string

--- strformat.js
+++ strformat.js
@@ -20,76 +20,80 @@
  * IN THE SOFTWARE.
  */
 
-(function (isNode, isAngular) {
+(function(isNode, isAngular) {
+  // Matches '{{', '}}', and '{<token>}' where <token> is one or more
+  // word characters (letter, number, or underscore).
+  var RE = /\{\{|\}\}|\{([^\}]+)\}/g;
 
-    // Matches '{{', '}}', and '{<token>}' where <token> is one or more
-    // word characters (letter, number, or underscore).
-    var RE = /\{\{|\}\}|\{([^\}]+)\}/g;
-
-    // Gets a property from an object by string.
-    function getProperty(o, s) {
-        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
-        s = s.replace(/^\./, ''); // strip a leading dot
-        var a = s.split('.');
-        while (a.length) {
-            var n = a.shift();
-            if (n in o) {
-                o = o[n];
-            } else {
-                return;
-            }
-        }
-        return o;
+  // Gets a property from an object by string.
+  function getProperty(o, s) {
+    s = s.replace(/\[(\w+)\]/g, ".$1"); // convert indexes to properties
+    s = s.replace(/^\./, ""); // strip a leading dot
+    var a = s.split(".");
+    while (a.length) {
+      var n = a.shift();
+      if (n in o) {
+        o = o[n];
+      } else {
+        return;
+      }
     }
+    return o;
+  }
 
-    /**
-     * Formats the specified string by replacing placeholders in the
-     * string with replacement values from the function arguments.
-     * Placeholders are specified with curly braces.
-     *
-     * The arguments can be a list of values, an array, or an object.
-     * For a list of values or an array, numeric placeholders are used.
-     * For an object, property name placeholders are used.
-     *
-     * Placeholders are escaped by doubling them (e.g., {{0}}).
-     * Any placeholder not matching an argument is left alone.
-     *
-     * strformat('Error {0}: {1}', 404, 'Not Found');
-     * > 'Error 404: Not Found' (uses argument position placeholders)
-     *
-     * strformat('{0}, {1}, and {2}', ['Red', 'Green', 'Blue']);
-     * > 'Red, Green, and Blue' (uses array index placeholders)
-     *
-     * strformat('Hi {first} {last}', {first: 'John', last: 'Doe'});
-     * > 'Hi John Doe' (uses object property name placeholders)
-     */
-    function strformat(str, args) {
-        args = Array.prototype.slice.call(arguments, 1);
-        if (args.length < 1) {
-            return str; // nothing to replace
-        } else if ((args.length < 2) && (typeof args[0] === 'object')) {
-            args = args[0]; // handle a single array or object
-        }
-        return str.replace(RE, function (m, n) {
-            if (m == '{{') {
-                return '{';
-            }
-            if (m == '}}') {
-                return '}';
-            }
-            var val = getProperty(args, n);
-            return (typeof val === 'undefined') ? m : val;
-        });
-    }
+  /**
+   * Formats the specified string by replacing placeholders in the
+   * string with replacement values from the function arguments.
+   * Placeholders are specified with curly braces.
+   *
+   * The arguments can be a list of values, an array, or an object.
+   * For a list of values or an array, numeric placeholders are used.
+   * For an object, property name placeholders are used.
+   *
+   * Placeholders are escaped by doubling them (e.g., {{0}}).
+   * Any placeholder not matching an argument is left alone.
+   *
+   * strformat('Error {0}: {1}', 404, 'Not Found');
+   * > 'Error 404: Not Found' (uses argument position placeholders)
+   *
+   * strformat('{0}, {1}, and {2}', ['Red', 'Green', 'Blue']);
+   * > 'Red, Green, and Blue' (uses array index placeholders)
+   *
+   * strformat('Hi {first} {last}', {first: 'John', last: 'Doe'});
+   * > 'Hi John Doe' (uses object property name placeholders)
+   */
+  function strformat(str, args, escapeValues) {
+    return str.replace(RE, function(m, n) {
+      if (m == "{{") {
+        return "{";
+      }
+      if (m == "}}") {
+        return "}";
+      }
+      var val = getProperty(args, n);
+      if (typeof val === "undefined") {
+        return "";
+      }
 
-    if (isNode) {
-        module.exports = strformat;
-    }
+      if (escapeValues) {
+        val = val.replace(/\'/g, "\\'");
+        val = val.replace(/\"/g, '\\"');
+      }
 
-    if (isAngular) {
-        angular.module('strformat', []).factory('strformat', function () {
-            return strformat;
-        });
-    }
+      return val;
+    });
+  }
 
-})(typeof module !== 'undefined' && module.exports, typeof angular !== 'undefined');
+  if (isNode) {
+    module.exports = strformat;
+  }
+
+  if (isAngular) {
+    angular.module("strformat", []).factory("strformat", function() {
+      return strformat;
+    });
+  }
+})(
+  typeof module !== "undefined" && module.exports,
+  typeof angular !== "undefined"
+);
